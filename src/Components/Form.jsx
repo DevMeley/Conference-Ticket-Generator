c09d3c0fe578a03979e4117548208c25e695373e
DevMeley <ayantolaAbisola@outlook.com>
2025-02-12 23:48:40 +0100

    second

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,74 @@
-import React from 'react'
-import FirstFormPage from './FirstFormPage'
-import SecondFormPage from './SecondFormPage'
-import LastFormPage from './LastFormPage'
-
+import React, { useState } from "react";
+import FirstFormPage from "./FirstFormPage";
+import SecondFormPage from "./SecondFormPage";
+import LastFormPage from "./LastFormPage";
 
 export default function Form() {
+  const [count, setCount] = useState(1);
+  const [ticketType, setTicketType] = useState();
+  const [ticketNumber, setTicketNumber] = useState(1);
+  const [profilePhoto, setProfilePhoto] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [specialRequest, setSpecialRequest] = useState("");
+  const [barCode, setBarCode] = useState("");
+  const [error, setError] = useState("");
+
+  const stepCountIncrement = () => {
+    setCount(count + 1);
+  };
+  const stepCountDecrement = () => {
+    setCount(count - 1);
+  };
+  const cancel = () => {
+    setCount(1);
+  };
+
+  //   form handling
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log(name, email);
+
+    
+  };
+
   return (
     <div>
-        <FirstFormPage />
-        <SecondFormPage />
-        <LastFormPage />
+      <form onSubmit={handleForm}>
+        {count === 1 ? (
+          <FirstFormPage
+            callbackProp={stepCountIncrement}
+            callbackProp2={stepCountDecrement}
+            count={count}
+            cancelCallBack={cancel}
+            ticketType={ticketType}
+            ticketNumber={ticketNumber}
+            setTicketType={setTicketType}
+            setTicketNumber={setTicketNumber}
+          />
+        ) : count === 2 ? (
+          <SecondFormPage
+            callbackProp={stepCountIncrement}
+            callbackProp2={stepCountDecrement}
+            count={count}
+            profilePhoto={profilePhoto}
+            name={name}
+            email={email}
+            specialRequest={specialRequest}
+            setProfilePhoto={setProfilePhoto}
+            setName={setName}
+            setEmail={setEmail}
+            setSpecialRequest={setSpecialRequest}
+          />
+        ) : (
+          <LastFormPage
+            callbackProp={stepCountIncrement}
+            callbackProp2={stepCountDecrement}
+            count={count}
+            cancelCallBack={cancel}
+          />
+        )}
+      </form>
     </div>
-  )
+  );
 }
--- src/Components/SecondFormPage.jsx
+++ src/Components/SecondFormPage.jsx
@@ -1,43 +1,83 @@
-import React from 'react'
-import "../CSS/FirstFormPage.css"
-import "../CSS/SecondFormPage.css"
+import React from "react";
+import "../CSS/FirstFormPage.css";
+import "../CSS/SecondFormPage.css";
 
-export default function SecondFormPage() {
+
+export default function SecondFormPage({
+  callbackProp,
+  count,
+  callbackProp2,
+  profilePhoto,
+  email,
+  name,
+  specialRequest,
+  setProfilePhoto,
+  setName,
+  setEmail,
+  setSpecialRequest
+}) {
   return (
-    <div className='form-container'>
-            <div className="form-heading">
-                <h3>Attendee Details</h3>
-                <span>Step 2/3</span>
-            </div>
-        <div className="form-container-wrapper">
-            <div className="upload-photo-container form-wrapper">
-                <p>Upload Photo</p>
-                <div className="image-background">
-                    <div className="drag-and-drop">
-                        <p>Drag & drop or click to upload</p>
-                    </div>
-                </div>
-            </div>
-            <hr />
-            <div className="inputs">
-                <div className="input">
-                    <label>Enter your name</label>
-                    <input type="text" />
-                </div>
-                <div className="input">
-                    <label>Enter your Email</label>
-                    <input type="text" />
-                </div>
-                <div className="input">
-                    <label>Special Request</label>
-                    <textarea>Text area</textarea>
-                </div>
-            </div>
-            <div className="second-form-button">
-                <button className="back-btn">Back</button>
-                <button className="get-btn">Get my free ticket</button>
-            </div>
+    <div className="form-container">
+      <div className="form-heading">
+        <h3>Attendee Details</h3>
+        <span>Step {count}/3</span>
+      </div>
+      <div className="form-container-wrapper">
+        <div className="upload-photo-container form-wrapper">
+          <p>Upload Photo</p>
+          <div className="image-background">
+            {profilePhoto && (<img className="image-background" src={URL.createObjectURL(profilePhoto)}/>)}
+          </div>
+            <label className="drag-and-drop">
+              <p>Drag & drop or click to upload</p>
+              <input
+                type="file"
+                value={profilePhoto}
+                onChange={(e)=>setProfilePhoto(e.target.files[0])}
+              />
+            </label>
         </div>
+        <hr />
+        <div className="inputs">
+          <div className="input">
+            <label>Enter your name</label>
+            <input type="text" value={name} onChange={(e)=> setName(e.target.value)}/>
+          </div>
+          <div className="input">
+            <label>Enter your Email</label>
+            <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          </div>
+          <div className="input">
+            <label>Special Request</label>
+            <textarea
+                name="description"
+                value={specialRequest}
+                onChange={(e)=>setSpecialRequest(e.target.value)}
+                placeholder="Text area"
+                required
+            />
+          </div>
+        </div>
+        <div className="second-form-button">
+          <button
+            className="back-btn"
+            onClick={() => {
+              callbackProp2();
+            }}
+          >
+            Back
+          </button>
+          <button
+            type="submit"
+            className="get-btn"
+            onClick={() => {
+              callbackProp();
+            }}
+          >
+            Get my free ticket
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
